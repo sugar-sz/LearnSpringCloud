3dae7c1d8de626c02446c1d0f4b8a274319e583f
sugar-sz <luyujie840225521@gmail.com>
2020-12-22 22:47:01 +0800

    ribbon

--- cloud-provider-payment8002/src/main/java/com/luyujie/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/luyujie/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.luyujie.springcloud.entities.CommonResult;
 import com.luyujie.springcloud.entities.Payment;
 import com.luyujie.springcloud.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @PostMapping("/save")
     public CommonResult save(@RequestBody Payment payment){
@@ -20,5 +24,10 @@ public class PaymentController {
     @GetMapping("/get/{id}")
     public CommonResult get(@PathVariable("id") Long id){
         return paymentService.get(id);
+    }
+
+    @GetMapping("/lb")
+    public String getPaymentLB(){
+        return serverPort;
     }
 }
